8a171dab509f8e6bc32040ef976851d9da74594e
MateoOspina <35085462+MateoOspina@users.noreply.github.com>
2018-01-07 02:52:03 -0500

    Revert ":-1:"
    This reverts commit 69f55aadb53a82c703f6e25c2b009880e5d0cdf3.

--- ProyectoGrafos/src/clases/CarroLadron.java
+++ ProyectoGrafos/src/clases/CarroLadron.java
@@ -5,9 +5,6 @@
  */
 package clases;
 
-import java.awt.Image;
-import javax.swing.ImageIcon;
-
 /**
  *
  * @author Mateo
@@ -17,26 +14,5 @@ public class CarroLadron {
     int fila;
     int columna;
     int areaContacto;
-    String[] direccionesImgLadron;
-    Image[] ladronImg;
-    String direccionLadron;
-
-    public CarroLadron() {
-    }
-
-    public CarroLadron(int fila, int columna, int areaContacto) {
-        this.fila = fila;
-        this.columna = columna;
-        this.areaContacto = areaContacto;
-        this.direccionesImgLadron =new String []{"src/imagenes/Ladrones/Audi","src/imagenes/Ladrones/Black viper","src/imagenes/Ladrones/Car O","src/imagenes/Ladrones/Mini Truck","src/imagenes/Ladrones/Mini van","src/imagenes/Ladrones/Taxi"} ;
-        int dirRandom= (int) (Math.random() * 3);
-        String dirObtenida=this.direccionesImgLadron[dirRandom];
-        for (int i = 1; i <= 4; i++) {
-           this.ladronImg[i-1]=new ImageIcon(getClass().getResource(dirObtenida+"/"+i+".png")).getImage();
-        }
-        this.ladronImg = ladronImg;
-        this.direccionLadron = direccionLadron;
-    }
-    
 
 }
