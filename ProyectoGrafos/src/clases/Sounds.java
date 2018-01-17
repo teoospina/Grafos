12f89e63ff263843c887626952df12f2da6392bc
daniel16go <daniel.16.go@gmail.com>
2018-01-17 14:30:50 -0500

    sound

--- ProyectoGrafos/src/clases/Sounds.java
+++ ProyectoGrafos/src/clases/Sounds.java
@@ -4,14 +4,47 @@
  * and open the template in the editor.
  */
 package clases;
+
 import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import sun.audio.*;
+
+import java.io.File;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 
 /**
  *
  * @author Mateo
  */
-public class Sounds {
+public class Sounds{
+
+    
+    public static void fondoSound(String ruta) {
+        try {
+
+            AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File(ruta));
+            Clip clip = AudioSystem.getClip();
+            clip.open(inputStream);
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+            //Thread.sleep(5000);
+        } catch (Exception e) {
+        }
+    }
+    public static void radioSound(String ruta) {
+        try {
+
+            AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File(ruta));
+            Clip clip = AudioSystem.getClip();
+            clip.open(inputStream);
+            clip.start();
+            //Thread.sleep(5000);
+        } catch (Exception e) {
+        }
+    }
+
 
     /**
      *
@@ -19,10 +52,11 @@ public class Sounds {
      * @throws FileNotFoundException
      * @throws IOException
      */
-    public static void rep(String ruta) throws FileNotFoundException, IOException{
-       InputStream in= new FileInputStream(ruta);
-       AudioStream audio= new AudioStream(in);
-       AudioPlayer.player.start(audio);
+    public static void rep(String ruta) throws FileNotFoundException, IOException {
+        InputStream in = new FileInputStream(ruta);
+        AudioStream audio = new AudioStream(in);
+        AudioPlayer.player.start(audio);
     }
-        
-    }
+
+
+}
