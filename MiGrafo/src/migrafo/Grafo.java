37722b163db1f3aee2a0dc005cff94a2e9940f85
MateoOspina <35085462+MateoOspina@users.noreply.github.com>
2018-01-22 17:33:50 -0500

    Perro hp

--- MiGrafo/src/migrafo/Grafo.java
+++ MiGrafo/src/migrafo/Grafo.java
@@ -244,5 +244,7 @@ public class Grafo {
         }
         return false;
     }
+    
+    
 
 }
--- MiGrafo/src/migrafo/MiGrafo.java
+++ MiGrafo/src/migrafo/MiGrafo.java
@@ -32,22 +32,37 @@ public class MiGrafo {
 //        graf.conexo(6);//Recalcular
 //        graf.Grado();//Bien.
         
-        Grafo graRecoridos= new Grafo();
-        graRecoridos.preLLenado(6);
-        graRecoridos.llenadoMatrices("a", "b", 0, false);
-        graRecoridos.llenadoMatrices("a", "d", 0, false);
-        graRecoridos.llenadoMatrices("a", "e", 0, false);
-        graRecoridos.llenadoMatrices("b", "c", 0, false);
-        graRecoridos.llenadoMatrices("b", "e", 0, false);
-        graRecoridos.llenadoMatrices("c", "d", 0, false);
-        graRecoridos.llenadoMatrices("c", "f", 0, false);
-        graRecoridos.llenadoMatrices("d", "e", 0, false);
-        graRecoridos.llenadoMatrices("d", "f", 0, false);
-        graRecoridos.llenadoMatrices("e", "f", 0, false);
-        graRecoridos.imprimirMatrizAdy();
-        graRecoridos.anchura("a");
-        graRecoridos.profundidad("a");
-        graRecoridos.fuerteConectado();
+//        Grafo graRecoridos= new Grafo();
+//        graRecoridos.preLLenado(6);
+//        graRecoridos.llenadoMatrices("a", "b", 0, false);
+//        graRecoridos.llenadoMatrices("a", "d", 0, false);
+//        graRecoridos.llenadoMatrices("a", "e", 0, false);
+//        graRecoridos.llenadoMatrices("b", "c", 0, false);
+//        graRecoridos.llenadoMatrices("b", "e", 0, false);
+//        graRecoridos.llenadoMatrices("c", "d", 0, false);
+//        graRecoridos.llenadoMatrices("c", "f", 0, false);
+//        graRecoridos.llenadoMatrices("d", "e", 0, false);
+//        graRecoridos.llenadoMatrices("d", "f", 0, false);
+//        graRecoridos.llenadoMatrices("e", "f", 0, false);
+//        graRecoridos.imprimirMatrizAdy();
+//        graRecoridos.anchura("a");
+//        graRecoridos.profundidad("a");
+//        graRecoridos.fuerteConectado();
+
+Grafo grafoConex= new Grafo();
+grafoConex.preLLenado(6);
+grafoConex.llenadoMatrices("a", "e", 6, true);
+grafoConex.llenadoMatrices("a", "f", 2, true);
+grafoConex.llenadoMatrices("b", "a", 3, true);
+grafoConex.llenadoMatrices("b", "e", 9, true);
+grafoConex.llenadoMatrices("b", "d", 4, true);
+grafoConex.llenadoMatrices("f", "e", 4, true);
+grafoConex.llenadoMatrices("d", "e", 7, true);
+grafoConex.llenadoMatrices("c", "a", 10, true);
+grafoConex.llenadoMatrices("c", "b", 5, true);
+grafoConex.llenadoMatrices("c", "d", 11, true);
+grafoConex.imprimirMatrizAdy();
+grafoConex.fuerteConectado();
     }
     
 }
